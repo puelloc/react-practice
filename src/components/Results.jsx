f1b9ed91714e8f87595ae7794ca6359179ad2a16
Cristopher <cristopherjpuello@gmail.com>
2024-08-08 16:07:38 -0500

    moved recommendation-selection and recommendation-view from ResultsView to Results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import './Results.css';
 import ResultTitle from './ResultTitle.jsx';
-import ResultsView from './ResultsView.jsx';
+import RecommendationSelection from './RecommendationSelection';
+import RecommendationView from './RecommendationView';
+import './Results.css';
+
+const Type = {
+  COLUMN: 'column',
+  MODEL: 'model'
+};
 
 const Results = () => {
+  const [type, setType] = useState(null);
+  const [name, setName] = useState('');
+  const [recommendations, setRecommendations] = useState({});
   return (
     <>
         <div className="result-title"><ResultTitle/></div>
-        <div className="results-view"><ResultsView/></div>
+        <div className="results-container">
+          <div className="recommendation-selection">
+            <RecommendationSelection />
+          </div>
+          <div className="recommendation-view">
+            <RecommendationView />
+          </div>
+        </div>
     </>
   );
 }
